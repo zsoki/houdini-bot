12612477320c6c0f0aa1d98c2fe2730fe31b2772
zsoki <tegyukfel@gmail.com>
2021-05-31 13:56:28 +0200

    Migrate to Kord

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,42 +1,41 @@
 group = "hu.zsoki.houdinibot.app"
 version = "0.1.0"
 
-plugins {
-    kotlin("plugin.serialization") version "1.4.10"
-}
-
 application {
     mainClassName = "hu.zsoki.houdinibot.app.HoudiniBotKt"
 }
 
 repositories {
     maven(url = "https://jitpack.io")
+    maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
 }
 
 dependencies {
     // Kotlin
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.10")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
 
     // Logging
-    implementation("io.github.microutils:kotlin-logging:2.0.3")
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.13.3")
+    implementation("io.github.microutils:kotlin-logging:2.0.6")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.14.1")
 
     // Discord
-    implementation("com.serebit.strife", "strife-client-jvm", "0.4.0")
+    implementation("dev.kord:kord-core:kotlin-1.5") {
+        version {
+            strictly("kotlin-1.5-SNAPSHOT")
+        }
+    }
 
     // Persistence
-    implementation("org.xerial:sqlite-jdbc:3.32.3.2")
-    implementation("org.jetbrains.exposed", "exposed-core", "0.22.1")
-    implementation("org.jetbrains.exposed", "exposed-jdbc", "0.22.1")
-    implementation("org.jetbrains.exposed", "exposed-java-time", "0.22.1")
-//    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.1")
+    implementation("org.xerial:sqlite-jdbc:3.34.0")
+    implementation("org.jetbrains.exposed", "exposed-core", "0.31.1")
+    implementation("org.jetbrains.exposed", "exposed-jdbc", "0.31.1")
+    implementation("org.jetbrains.exposed", "exposed-java-time", "0.31.1")
 
     // Testing
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.0")
-    testImplementation("io.mockk:mockk:1.10.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.2")
+    testImplementation("io.mockk:mockk:1.11.0")
 }
 
 tasks {
--- app/src/main/kotlin/hu/zsoki/houdinibot/app/HoudiniBot.kt
+++ app/src/main/kotlin/hu/zsoki/houdinibot/app/HoudiniBot.kt
@@ -1,23 +1,18 @@
 package hu.zsoki.houdinibot.app
 
-import com.serebit.strife.bot
-import com.serebit.strife.data.Activity
-import com.serebit.strife.data.OnlineStatus
-import com.serebit.strife.entities.field
-import com.serebit.strife.entities.reply
-import com.serebit.strife.entities.title
-import hu.zsoki.houdinibot.app.Invite.generateInviteUrl
-import hu.zsoki.houdinibot.app.command.commandStore
-import hu.zsoki.houdinibot.app.db.*
+import dev.kord.core.Kord
+import dev.kord.core.behavior.reply
+import dev.kord.core.event.message.MessageCreateEvent
+import dev.kord.core.on
 import hu.zsoki.houdinibot.app.db.Db.driverClass
 import hu.zsoki.houdinibot.app.db.Db.jdbcUrl
+import hu.zsoki.houdinibot.app.db.QuoteTable
 import hu.zsoki.houdinibot.app.domain.truncate
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.TransactionManager
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import java.sql.Connection
-import java.time.Duration
 
 private val token = System.getProperty("discord.token") ?: error("You need to define discord.token property.")
 
@@ -29,65 +24,71 @@ suspend fun main() {
         SchemaUtils.create(QuoteTable)
     }
 
-    bot(token) {
-        generateInviteUrl()
+    val client = Kord(token)
 
-        scheduledTask("updateUptime", Duration.ofMinutes(5)) {
-            context.updatePresence(OnlineStatus.ONLINE, Activity.Type.Playing to ";help | ${Uptime.formattedString}")
-        }
+    client.on<MessageCreateEvent> {
+        when (message.content) {
+            ";invite" -> message.reply { content = Invite.getUrl(client.selfId.asString) }
+            ";quotes" -> {
+                val quotes = QuoteTable.getAllQuotes()
 
-        commandStore {
-            prefix(";") {
-                command("help") {
-                    message.reply {
-                        title("Commands")
-                        field("`;invite`") { "Posts the invite URL" }
-                        field("`.. <keyword> text to store as quote`") { "Store text of any length as a quote for <keyword>. The <keyword> doesn't need to exist. If you add it to an already existing one, the quote will be chosen randomly when recalling." }
-                        field("`... <keyword>`") { "Recall quote for <keyword>" }
-                        field("`-- <quote-id>`") { "Remove quote identified by the <quote-id>. You can get the ID when recalling a quote. E.g.: `#123`" }
-                        field("`;quotes <keyword (optional)>`") { "List quotes - if no <keyword> is provided, then list all quotes stored." }
-                    }
+                val quotesReply = quotes.joinToString("\n") { quote ->
+                    "`#${quote.id} ${quote.keyword}` 📢  ${quote.text.truncate()}"
                 }
 
-                command("invite") {
-                    message.reply(Invite.url)
-                }
-
-                command("quotes") {
-                    val quotes = if (words.isNullOrEmpty()) QuoteTable.getAllQuotes() else QuoteTable.getAllQuotesFor(words[0])
-
-                    val quotesReply = quotes.joinToString("\n") { quote ->
-                        "`#${quote.id} ${quote.keyword}` 📢  ${quote.text.truncate()}"
-                    }
-
-                    message.reply(quotesReply)
-                }
+                message.reply { content = quotesReply.ifBlank { "No quotes yet." } }
             }
-
-            command("..") {
-                if (extra == null) return@command
-
-                val authorName = message.getAuthor()?.getUsername() ?: "UnknownUser"
-                val id = QuoteTable.addQuote(authorName, words[0], extra.removePrefix(words[0].trim()))
-                message.reply {
-                    description = """
-                        Quote added successfully.
-                        Type `... ${words[0]}` to recall, `-- $id` to remove.
-                        """.trimIndent()
-                }
-            }
-
-            command("...") {
-                val quote = QuoteTable.getRandomQuote(words[0])
-                message.reply("`#${quote.id}` 📢  ${quote.text}")
-            }
-
-            command("--") {
-                val quote = QuoteTable.removeQuote(words[0].toInt())
-                message.reply {
-                    description = "Removed quote `#${quote.id}` from `${quote.keyword}` pool."
-                }
-            }
+            ";help" -> message.reply { content = "No help ready. Currently migrating to Kord." }
         }
     }
+
+    client.login()
+
+//    bot(token) {
+//        generateInviteUrl()
+//
+//        scheduledTask("updateUptime", Duration.ofMinutes(5)) {
+//            context.updatePresence(OnlineStatus.ONLINE, Activity.Type.Playing to ";help | ${Uptime.formattedString}")
+//        }
+//
+//        commandStore {
+//            prefix(";") {
+//                command("help") {
+//                    message.reply {
+//                        title("Commands")
+//                        field("`;invite`") { "Posts the invite URL" }
+//                        field("`.. <keyword> text to store as quote`") { "Store text of any length as a quote for <keyword>. The <keyword> doesn't need to exist. If you add it to an already existing one, the quote will be chosen randomly when recalling." }
+//                        field("`... <keyword>`") { "Recall quote for <keyword>" }
+//                        field("`-- <quote-id>`") { "Remove quote identified by the <quote-id>. You can get the ID when recalling a quote. E.g.: `#123`" }
+//                        field("`;quotes <keyword (optional)>`") { "List quotes - if no <keyword> is provided, then list all quotes stored." }
+//                    }
+//                }
+//            }
+//
+//            command("..") {
+//                if (extra == null) return@command
+//
+//                val authorName = message.getAuthor()?.getUsername() ?: "UnknownUser"
+//                val id = QuoteTable.addQuote(authorName, words[0], extra.removePrefix(words[0].trim()))
+//                message.reply {
+//                    description = """
+//                        Quote added successfully.
+//                        Type `... ${words[0]}` to recall, `-- $id` to remove.
+//                        """.trimIndent()
+//                }
+//            }
+//
+//            command("...") {
+//                val quote = QuoteTable.getRandomQuote(words[0])
+//                message.reply("`#${quote.id}` 📢  ${quote.text}")
+//            }
+//
+//            command("--") {
+//                val quote = QuoteTable.removeQuote(words[0].toInt())
+//                message.reply {
+//                    description = "Removed quote `#${quote.id}` from `${quote.keyword}` pool."
+//                }
+//            }
+//        }
+//    }
 }
